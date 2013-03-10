24a0e44b4be4541457554500c93e2797186dd598
Zetoke <zetoke@gmail.com>
2013-03-11 03:40:21 +0400

    fix: code-style

--- blog/urls.py
+++ blog/urls.py
@@ -18,13 +18,8 @@ urlpatterns = patterns('',
   
     (r'^$', 'blog.views.index'),
 
-    url(r'^blog/view/(?P<slug>[^\.]+).html', 
-    'blog.views.view_post', 
-    name='view_blog_post'),
+    url(r'^blog/view/(?P<slug>[^\.]+).html', 'blog.views.view_post', name='view_blog_post'),
 
-    url(r'^blog/category/(?P<slug>[^\.]+).html', 
-    'blog.views.view_category', 
-    name='view_blog_category'),
-
-    (r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^blog/category/(?P<slug>[^\.]+).html', 'blog.views.view_category',
+        name='view_blog_category'), (r'^comments/', include('django.contrib.comments.urls')),
 )
